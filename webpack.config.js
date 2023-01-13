a6b36afba0674bb380ee7bbcf4a6dddf44d06900
Tadeu Brasil <tadeubrasill@gmail.com>
2023-01-13 16:56:29 -0300

    iniciando o repositorio

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
+
+const dirApp = path.join(__dirname, 'app');
+const dirShared = path.join(__dirname, 'shared');
+const dirStyles = path.join(__dirname, 'styles');
+const dirNode = 'node_modules';
+
+module.exports = {
+  entry:  [
+    path.join(dirApp, 'index.js'),
+    path.join(dirStyles, 'index.scss')
+  ],
+
+  resolve: {
+    modules: [
+      dirApp,
+      dirShared,
+      dirStyles,
+      dirNode
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './shared',
+          to: ''
+        }
+      ]
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+
+  module:{
+    rules:[
+      {
+        test: /\.js$/,
+        use:{
+          loader: 'babel-loader'
+        }
+      },
+
+      {
+        test: /\.scss$/,
+        use :[
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options:{
+              publicPath: ''
+            }
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+        loader: 'file-loader',
+        options: {
+          name(file){
+            return '[name].[hash].[ext]'
+          }
+        }
+      }
+    ]
+  }
+
+}
