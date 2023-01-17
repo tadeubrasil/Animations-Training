17d401e96e87eeba5fa0329b13a61353d7810795
Tadeu Brasil <tadeubrasill@gmail.com>
2023-01-17 17:28:07 -0300

    criação da views e pages

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('<strong>Hello World!</strong>')
+  res.render('pages/home')
+})
+
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/collections', (req, res) => {
+  res.render('pages/collections')
+})
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail')
 })
 
 app.listen(port, () => {
