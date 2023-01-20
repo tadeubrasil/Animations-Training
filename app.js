9c04c6c5b8cef3646df83904e3f7fa5a93fa3280
Tadeu Brasil <tadeubrasill@gmail.com>
2023-01-20 14:52:55 -0300

    integração com o prismic.io e criação da página about

--- app.js
+++ app.js
@@ -1,27 +1,98 @@
-const express = require('express')
-const app = express()
+require('dotenv').config()
+
+const fetch = require('node-fetch')
 const path = require('path')
-const port = 3000
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const Prismic = require('@prismicio/client')
+const PrismicH = require('@prismicio/helpers')
+
+// Initialize the prismic.io api
+const initApi = (req) => {
+  return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
+    fetch
+})
+}
+
+const HandleLinkResolver = (doc) => {
+  return '/'
+}
+// Middleware to inject prismic context
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: HandleLinkResolver
+  }
+  res.locals.PrismicH = PrismicH
+
+  next()
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  res.render('pages/home')
+const handleRequest = async (api) => {
+  const [about] = await Promise.all([
+    api.getSingle('about'),
+    api.query(Prismic.Predicates.at('document.type', 'collection'), {
+      fetchLinks: 'product.image'
+    })
+  ])
+
+  const assets = []
+  about.data.gallery.forEach((item) => {
+    assets.push(item.image.url)
+  })
+
+  about.data.body.forEach((section) => {
+    if (section.slice_type === 'gallery') {
+      section.items.forEach((item) => {
+        assets.push(item.image.url)
+      })
+    }
+  })
+
+  return {
+    about
+  }
+}
+
+app.get('/', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/home', {
+    ...defaults
+  })
 })
 
-app.get('/about', (req, res) => {
-  res.render('pages/about')
+app.get('/about', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/about', {
+    ...defaults
+  })
 })
 
-app.get('/collections', (req, res) => {
-  res.render('pages/collections')
+app.get('/collection', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/collection', {
+    ...defaults
+  })
 })
 
-app.get('/detail/:id', (req, res) => {
+app.get('/detail/:uid', (req, res) => {
   res.render('pages/detail')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port  http://localhost:${port}`)
 })
